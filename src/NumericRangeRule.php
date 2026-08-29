2459578422dcefb6b303ffd7c6ca842a0b89e36d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-29 15:59:00 +0400

    [AEGISORA-69380243] Implemented private construct.

--- src/NumericRangeRule.php
+++ src/NumericRangeRule.php
@@ -16,4 +16,22 @@ class NumericRangeRule
 
     private bool $minInclusive;
     private bool $maxInclusive;
+
+    /**
+     * @param numeric|null $min
+     * @param numeric|null $max
+     * @param bool $minInclusive
+     * @param bool $maxInclusive
+     */
+    private function __construct(
+        $min,
+        $max,
+        bool $minInclusive,
+        bool $maxInclusive
+    ) {
+        $this->min = $min;
+        $this->max = $max;
+        $this->minInclusive = $minInclusive;
+        $this->maxInclusive = $maxInclusive;
+    }
 }
