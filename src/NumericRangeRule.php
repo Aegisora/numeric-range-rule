f0580fc03647194c7c976cec66b06218c99786b6
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-08-29 15:57:34 +0400

    [AEGISORA-69380243] Implemented attributes.

--- src/NumericRangeRule.php
+++ src/NumericRangeRule.php
@@ -4,4 +4,16 @@ namespace Aegisora\Rules;
 
 class NumericRangeRule
 {
+    /**
+     * @var numeric|null
+     */
+    private $min;
+
+    /**
+     * @var numeric|null
+     */
+    private ?int $max;
+
+    private bool $minInclusive;
+    private bool $maxInclusive;
 }
